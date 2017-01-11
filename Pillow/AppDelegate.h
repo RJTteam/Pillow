fe3b000ddae9726f8c8fd1084820ffc8c409211a
Yazhong Luo <kleistluo@gmail.com>
2017-01-11 15:41:16 -0600

    Initial Commit

---
+++ Pillow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pillow
+//
+//  Created by Lucas Luo on 1/11/17.
+//  Copyright © 2017 RJT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pillow/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Pillow
+//
+//  Created by Lucas Luo on 1/11/17.
+//  Copyright © 2017 RJT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
